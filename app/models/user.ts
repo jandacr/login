62cdfd7161eb1e4150df84b1aef2307aba0a97da
ale.sorpresa28@gmail.com <ale.sorpresa28@gmail.com>
2016-10-28 03:03:45 -0600

    se crea el modelo o clase de usuario para dar de alta

---
+++ app/models/user.ts
@@ -0,0 +1,8 @@
+//clase para las propiedades de usuario, esto probablemente deba ir en el paquete usuario, esta por verse
+export class User {
+    username: string;
+    password: string;
+    firstname: string;
+    lastname: string;
+}
+//se agregaran mas claro, si el registro de usuarios ocupa mas datos, como direccion
