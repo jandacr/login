87ff640a523ef6e6072e2e07794a22bd073ab4e8
ale.sorpresa28@gmail.com <ale.sorpresa28@gmail.com>
2016-10-28 03:54:51 -0600

    partesitas del login vaya

---
+++ app/login/login.component.ts
@@ -0,0 +1,40 @@
+// usa el authentication service para el login/out. 
+// It automatically logs the user out when it initializes (ngOnInit) so the login page can also be used to logout.
+// Luego lo traduzco
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+ 
+import { AlertService, AuthenticationService } from '../_services/index';
+ 
+@Component({
+    moduleId: module.id,
+    templateUrl: 'login.component.html'
+})
+ 
+export class LoginComponent implements OnInit {
+    model: any = {};
+    loading = false;
+ 
+    constructor(
+        private router: Router,
+        private authenticationService: AuthenticationService,
+        private alertService: AlertService) { }
+ 
+    ngOnInit() {
+        // reset login status
+        this.authenticationService.logout();
+    }
+ 
+    login() {
+        this.loading = true;
+        this.authenticationService.login(this.model.username, this.model.password)
+            .subscribe(
+                data => {
+                    this.router.navigate(['/']);
+                },
+                error => {
+                    this.alertService.error(error);
+                    this.loading = false;
+                });
+    }
+}
