b9f0339ad727817086983ed435d1969699e2c24b
ale.sorpresa28@gmail.com <ale.sorpresa28@gmail.com>
2016-10-28 03:39:59 -0600

    partes del inicio o home, interesantes

---
+++ app/home/home.component.ts
@@ -0,0 +1,31 @@
+// este componente es el que jala desde el almacenamiento local para el user service y el template, o sea el html
+import { Component, OnInit } from '@angular/core';
+ 
+import { User } from '../_models/index';
+import { UserService } from '../_services/index';
+ 
+@Component({
+    moduleId: module.id,
+    templateUrl: 'home.component.html'
+})
+ 
+export class HomeComponent implements OnInit {
+    currentUser: User;
+    users: User[] = [];
+ 
+    constructor(private userService: UserService) {
+        this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    }
+ 
+    ngOnInit() {
+        this.loadAllUsers();
+    }
+ 
+    deleteUser(id) {
+        this.userService.delete(id).subscribe(() => { this.loadAllUsers() });
+    }
+ 
+    private loadAllUsers() {
+        this.userService.getAll().subscribe(users => { this.users = users; });
+    }
+}
